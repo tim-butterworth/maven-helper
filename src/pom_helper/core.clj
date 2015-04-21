e91d883265d6e998bebc2fcb824c7d5319ac9e27
Timothy D Butterworth <timothy.d.butterworth@gmail.com>
2015-04-21 06:48:58 -0400

    Adds path-find capability to pom helper

--- src/pom_helper/core.clj
+++ src/pom_helper/core.clj
@@ -1,12 +1,37 @@
 (ns pom-helper.core
-  (:gen-class)
-  (:require [clojure.xml :as xml
-             clojure.zip :as zip]))
+;  (:gen-class)
+  (:require [clojure.xml :as xml]
+   :require [clojure.zip :as zip]))
 
 (defn parse-str [s]
-  (let [input (java.io.ByteArrayInputStream. (.getBytes s))]
+  (let [input (java.io.ByteArrayInputStream. (.getBytes (slurp s)))]
     (-> input (xml/parse) (zip/xml-zip))))
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defn find-in-list [key lst]
+  (if (empty? lst)
+    nil
+    (let [current (first lst)]
+      (if (= key (name (current :tag)))
+        current
+        (find-in-list key (rest lst))))))
+
+(defn unwrap-if-needed [data]
+  (if (or 
+       (sequential? data)
+       (empty? data))
+    data
+    (data :content)))
+
+(defn path-find [path data]
+  (let [node (unwrap-if-needed data)]
+    (if (empty? path)
+      node
+      (let [key (first path)]
+        (path-find (rest path) (find-in-list key node))))))
+;/Users/timothybutterworth/.m2/repository/org/springframework/spring-jpa/2.0.8/spring-jpa-2.0.8.pom
+
+;(defn -main [& args] (println "Hello, World!"))
+
+
+
+
--- test/pom_helper/core_test.clj
+++ test/pom_helper/core_test.clj
@@ -3,5 +3,25 @@
             [pom-helper.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "find-in-list"
+    (is (= nil (find-in-list "kewl" [])))
+    (is (= {:tag :kewl} (find-in-list "kewl" [{:tag :kewl}])))
+    (is (= {:tag :kewl} (find-in-list "kewl" [{:tag :not-it-1}
+                                              {:tag :not-it-2}
+                                              {:tag :not-it-3}
+                                              {:tag :kewl}])))
+    (is (= {:tag :kewl} (find-in-list "kewl" [{:tag :not-it-1}
+                                              {:tag :kewl}
+                                              {:tag :not-it-2}
+                                              {:tag :not-it-3}])))
+    (is (= nil (find-in-list "not-there" [{:tag :not-it-1}
+                                          {:tag :kewl}
+                                          {:tag :not-it-2}
+                                          {:tag :not-it-3}]))))
+  (testing "unwrap-if-needed"
+    (is (= nil (unwrap-if-needed nil)))
+    (is (= [] (unwrap-if-needed [])))
+    (is (= {:tag :kewl} (unwrap-if-needed {:content {:tag :kewl}}))))
+  (testing "path-find" 
+    (is (= nil (path-find ["not-there"] [{:tag :data}])))
+    (is (= [{:kewl :stuff}] (path-find ["data"] [{:tag :data :content [{:kewl :stuff}]}])))))
